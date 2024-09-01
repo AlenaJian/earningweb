03e7705fd2bee7c8a0eb1854c9d9027605100b02
shabkhanshab <komal8561829@gmail.com>
2024-09-01 12:24:35 +0530

    change

--- netlify/functions/EmailSend/EmailSend.js
+++ netlify/functions/EmailSend/EmailSend.js
@@ -1,6 +1,8 @@
 
 const nodemailer = require('nodemailer')
 
+
+
 const transport = nodemailer.createTransport({
 
   host: process.env.HOST,
@@ -12,9 +14,23 @@ const transport = nodemailer.createTransport({
       pass: process.env.USER_PASS,
   },
   
+  
 });
 
 const handler = async (event) => {
+    const headers = {
+        "Access-Control-Allow-Origin": "http://localhost:3000", // Update to specific origins if needed
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      };
+      if (event.httpMethod === 'OPTIONS') {
+        return {
+          statusCode: 200,
+          headers,
+          body: JSON.stringify({ message: "CORS preflight request success" }),
+        };
+      }
+
   console.log("emaillll",event.body)
   const { email, subject, text } = JSON.parse(event.body);
   console.log("check",email, subject, text)
@@ -26,16 +42,26 @@ const handler = async (event) => {
   };
 
   try {
-      await transport.sendMail(mailOptions);
-      return {
-          statusCode: 200,
-           headers: {
-                "Access-Control-Allow-Origin": "*",
-            },
-          body: JSON.stringify({
-              message: 'Email sent successfully',
-          }),
-      };
+       transport.sendMail(mailOptions).then(()=>{
+        return {
+            statusCode: 200,
+             headers: {
+                  "Access-Control-Allow-Origin": "*",
+              },
+            body: JSON.stringify({
+                message: 'Email sent successfully',
+            }),
+        };
+       }).catch((Err)=>{
+        console.log('error', err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'Please try after sometime',
+            }),
+        };
+       });
+     
   } catch (err) {
       console.log('error', err);
       return {
